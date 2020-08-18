85cb52a8c7b26b9a07632796bbb4303ca831ea67
AceOfHeart5 <>
2020-08-18 16:41:10 -0400

    fix dist build not working right

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist', // Folder to store generated bundle
     filename: 'bundle.js',  // Name of generated bundle after build
-    //publicPath: '/' // public URL of the output directory when referenced in a browser
+    publicPath: '/' // public URL of the output directory when referenced in a browser
   },
   module: {  // where we defined file patterns and their loaders
       rules: [ 
